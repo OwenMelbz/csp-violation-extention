07d106a85b03d76b405f94fc83d163aca56fab37
Owen Melbourne <owen@stormcreative.co.uk>
2021-02-17 13:30:12 +0000

    💥 Change - removed sourcemaps

--- unpacked/scripts/contentscript.js
+++ unpacked/scripts/contentscript.js
@@ -1,2 +1,1 @@
 "use strict";window.addEventListener("securitypolicyviolation",function(e){var i={},r=["blockedURI","documentURI","effectiveDirective","violatedDirective","originalPolicy","sourceFile","referrer","type"];r.forEach(function(r){i[r]=e[r]}),chrome.runtime.sendMessage(i)});
-//# sourceMappingURL=contentscript.js.map
